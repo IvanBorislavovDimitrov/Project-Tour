d62fb73d82efddb630bb55b1094a4e65918cd66d
Iskrova <mary.iskrova@gmail.com>
2018-07-08 14:11:43 +0300

    JPA annotations to entities added, some improvements on view

---
+++ reservations/src/main/java/app/controllers/HotelsRestController.java
@@ -0,0 +1,61 @@
+package app.controllers;
+
+import app.entities.Hotel;
+import app.repostiories.HibernateRepository;
+import app.repostiories.base.GenericRepository;
+import app.services.api.HotelsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class HotelsRestController {
+    private final HotelsService hotelsService;
+
+    @Autowired
+    public HotelsRestController(HotelsService hotelsService){
+        this.hotelsService = hotelsService;
+
+    }
+
+//    @RequestMapping("/hotels")
+//    public List<Hotel> getHotels(){
+//
+//        return hotelsService.getAllHotels();
+//    }
+
+    @RequestMapping("/hotels")
+    public List<Hotel> getHotelsByDestination(
+            @RequestParam(required = false) String destination,
+            @RequestParam(required = false) String page
+            ){
+       if(destination== null){
+           if(page.equals("")){
+               return hotelsService.getAllHotels();
+           }
+           else {
+               return hotelsService.getAllHotelsByPage(Integer.parseInt(page));
+           }
+
+       }else {
+           if(page.equals("")){
+               return hotelsService.getHotelsByDestination(destination);
+           }
+           else {
+               return hotelsService.getHotelsByDestinationAndPage(destination, Integer.parseInt(page));
+           }
+       }
+
+    }
+
+    @RequestMapping("/hotels/{id}")
+    public Hotel getHotelDetails(@PathVariable("id") String id){
+        return hotelsService.getHotelById(Integer.parseInt(id));
+    }
+
+}
---
+++ reservations/src/test/java/HotelServiceImplTest.java
@@ -0,0 +1,54 @@
+import app.entities.Hotel;
+import app.repostiories.base.GenericRepository;
+import app.services.imp.HotelServiceImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HotelServiceImplTest {
+    @Test
+    public void getAllHotels_whenHotelsAreAvaible_expectTheHotels() {
+        // Arrange
+        List<Hotel> products = Arrays.asList(
+                new Hotel(),
+                new Hotel()
+        );
+
+        GenericRepository<Hotel> repo = new GenericRepository<Hotel>() {
+            @Override
+            public List<Hotel> getAll() {
+                return products;
+            }
+
+            @Override
+            public Hotel getById(int id) {
+                return null;
+            }
+
+            @Override
+            public Hotel create(Hotel entity) {
+                return null;
+            }
+
+            @Override
+            public void setEntityClass(Class<Hotel> hotelClass) {
+
+            }
+        };
+
+        HotelServiceImpl service = new HotelServiceImpl(repo);
+
+        // Act
+
+        List<Hotel> actualProducts = service.getAllHotels();
+
+        // Assert
+
+        Assert.assertArrayEquals(
+                actualProducts.toArray(),
+                products.toArray()
+        );
+    }
+}
