13868d6f4bcd61058cd05dddc59953b0e21429f9
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2018-07-11 01:38:20 +0300

    Authentication admin

--- reservations/src/main/java/app/controllers/HotelsController.java
+++ reservations/src/main/java/app/controllers/HotelsController.java
@@ -4,6 +4,7 @@ import app.entities.Hotel;
 import app.model.dtos.HotelDto;
 import app.services.api.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,12 +24,14 @@ public class HotelsController {
     }
 
     @GetMapping("/createHotel")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String createHotel() {
 
         return "createHotel";
     }
 
     @PostMapping("createHotel")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String createHotel(HotelDto hotelDto) {
         this.hotelsService.createHotel(hotelDto);
 
